92cc1e1d4ab28a627b2de38697ae950cf3904c0c
kongil <o_o0122@naver.com>
2022-08-10 06:16:01 +0900

    로그인 처리하기 - 서블릿 HTTP 세션

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,11 @@
+package hello.login.web;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
+
+/*
+public interface SessionConst {
+    String LOGIN_MEMEBER = "loginMember";
+}
+ */
